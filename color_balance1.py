f091ba4751c9891693a62f33cd511f73bfb9888d
srashmi032 <srashmi032@gmail.com>
2018-06-01 20:55:43 +0530

    Color balance

---
+++ color_balance1.py
@@ -0,0 +1,33 @@
+from __future__ import (
+    division, absolute_import, print_function, unicode_literals)
+
+import cv2 as cv
+import numpy as np
+
+#Adjust pixel values 
+def show(final):
+    print('display')
+    cv.imshow('Result', final)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+
+
+img = cv.imread('w21.jpg')
+final = cv.cvtColor(img, cv.COLOR_BGR2LAB)
+
+avg_a = np.average(final[:, :, 1])
+avg_b = np.average(final[:, :, 2])
+
+for x in range(final.shape[0]):
+    for y in range(final.shape[1]):
+        l, a, b = final[x, y, :]
+       
+        l *= 100 / 255.0
+        final[x, y, 1] = a - ((avg_a - 128) * (l / 100.0) * 0.98)
+        final[x, y, 2] = b - ((avg_b - 128) * (l / 100.0) * 0.98)
+
+final = cv.cvtColor(final, cv.COLOR_LAB2BGR)
+
+
+show(final)
+cv.imwrite('resultw21.jpg', final)
